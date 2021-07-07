4beb473b7db8bcf3c89ebd3f469244e96aa1f1e5
SymmatricDev1 <symmatricdev1@gmail.com>
2021-07-07 11:34:02 +0530

    Login page design added

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -5,13 +5,15 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { LoginHeaderComponent } from './login-header/login-header.component';
 import { LoginFooterComponent } from './login-footer/login-footer.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, LoginHeaderComponent, LoginFooterComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
